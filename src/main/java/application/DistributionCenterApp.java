5dcf3d0c06953ddf85fcaf390d2ba5cba383027e
João Pedro <nmjoaopedro22@gmail.com>
2024-07-05 09:53:26 -0300

    adiciona classes ShelterApp e DistributionCenterAPp

---
+++ src/main/java/application/DistributionCenterApp.java
@@ -0,0 +1,11 @@
+package application;
+
+import java.util.Scanner;
+
+public class DistributionCenterApp {
+    protected Scanner sc;
+
+    public DistributionCenterApp(Scanner sc) {
+        this.sc = sc;
+    }
+}
---
+++ src/main/java/application/ShelterApp.java
@@ -0,0 +1,11 @@
+package application;
+
+import java.util.Scanner;
+
+public class ShelterApp {
+    protected Scanner sc;
+
+    public ShelterApp(Scanner sc) {
+        this.sc = sc;
+    }
+}
