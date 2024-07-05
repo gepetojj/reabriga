e47a7696e6751fd1eb58b7cde76eb7bc825f2e73
João Pedro <nmjoaopedro22@gmail.com>
2024-07-05 09:05:27 -0300

    cria entidade CLI e instancia na Main

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,14 @@
 package application;
 
+import cli.CLI;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CLI cli = new CLI(sc);
+
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/cli/CLI.java
@@ -0,0 +1,11 @@
+package cli;
+
+import java.util.Scanner;
+
+public class CLI {
+    protected Scanner sc;
+
+    public CLI(Scanner sc) {
+        this.sc = sc;
+    }
+}
