3b8aa8903ce6d16bb544e77cf6e640bf8e4ee92a
João Pedro <nmjoaopedro22@gmail.com>
2024-07-14 23:09:02 -0300

    atualiza Seeding

--- src/main/java/persistence/Seeding.java
+++ src/main/java/persistence/Seeding.java
@@ -3,40 +3,36 @@ package persistence;
 import entities.*;
 import entities.enums.ClothingSize;
 import entities.enums.ClothingType;
+import jakarta.persistence.EntityManager;
 
 import java.time.Instant;
 import java.util.ArrayList;
 
 public class Seeding {
+    private static Inventory persistDC(EntityManager em, DistributionCenter dc) {
+        em.persist(dc);
+        var inv = new Inventory(dc);
+        em.persist(inv);
+        dc.setInventory(inv);
+        em.persist(dc);
+        return inv;
+    }
+
     public static void seed() {
         var em = JPAUtil.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
 
-        var dc1 = new DistributionCenter("Distribuidora Toledo", "Rua ABC, Arapiraca, Alagoas", "57000000");
-        em.persist(dc1);
-
-        var dcs = new ArrayList<String>();
-        dcs.add("Centro de ajuda Marques");
-        dcs.add("Armazém Guimarães");
-
-        for (var name : dcs) {
-            var dc = new DistributionCenter(name, "Rua XYZ", "57000000");
-            em.persist(dc);
-            var inv = new Inventory(dc);
-            dc.setInventory(inv);
-            em.persist(inv);
-            em.persist(dc);
-        }
+        var dc1 = new DistributionCenter("Centro de Distribuição Esperança", "Av. Boqueirão, 2450 - Igara, Canoas - RS", "92032-420");
+        var dc1_inv = persistDC(em, dc1);
+        var dc2 = new DistributionCenter("Centro de Distribuição Prosperidade", "Av. Borges de Medeiros, 1501 – Cidade Baixa, Porto Alegre - RS", "90119-900");
+        persistDC(em, dc2);
+        var dc3 = new DistributionCenter("Centro de Distribuição Reconstrução", "R. Dr. Décio Martins Costa, 312 - Vila Eunice Nova, Cachoeirinha - RS", "94920-170");
+        persistDC(em, dc3);
 
         var st1 = new Shelter("Abrigo Dois Irmãos", "Rua ABC, Maceió, Alagoas", "Nome do responsável", "82999999999", "email@gmail.com");
         var st2 = new Shelter("Abrigo Santa Cruz", "Rua DEF, Maceió, Alagoas", "Nome da responsável", "82999999999", "email@gmail.com");
         em.persist(st1);
         em.persist(st2);
-
-        var inv1 = new Inventory(dc1);
-        dc1.setInventory(inv1);
-        em.persist(inv1);
-        em.persist(dc1);
 
         var inv2 = new Inventory(st1);
         st1.setInventory(inv2);
@@ -47,19 +43,19 @@ public class Seeding {
         var item2 = new Item("camisa", "Camisa verde feminina", ClothingType.M, ClothingSize.L);
         var item3 = new Item("pao-de-queijo", "Pão de queijo de Minas", 1.0, "kg", Instant.now());
         var item4 = new Item("escova-de-dentes", "Escova de dentes");
-        item1.setInventory(inv1);
-        item2.setInventory(inv1);
-        item3.setInventory(inv1);
+        item1.setInventory(dc1_inv);
+        item2.setInventory(dc1_inv);
+        item3.setInventory(dc1_inv);
         item4.setInventory(inv2);
         em.persist(item1);
         em.persist(item2);
         em.persist(item3);
         em.persist(item4);
-        inv1.addItem(item1);
-        inv1.addItem(item2);
-        inv1.addItem(item3);
+        dc1_inv.addItem(item1);
+        dc1_inv.addItem(item2);
+        dc1_inv.addItem(item3);
         inv2.addItem(item4);
-        em.persist(inv1);
+        em.persist(dc1_inv);
         em.persist(inv2);
 
         em.getTransaction().commit();
